602773c84ed2f57c7f6eeb43f1d643a401615ed9
bishoe01 <bishoe01@gmail.com>
2023-06-28 16:07:25 +0900

    header hover animation

--- catchtable/src/components/Header.jsx
+++ catchtable/src/components/Header.jsx
@@ -4,6 +4,7 @@ import { FlexRow, PaddingX } from '../constants/style';
 import { Fade, Slide } from 'react-reveal';
 
 function Header() {
+    const LINK_STYLE = "hover:text-primary transition duration-200 ease-in-out"
     const navigate = useNavigate();
     return (
         <Slide top cascade>
@@ -11,13 +12,13 @@ function Header() {
                 <img className='cursor-pointer w-[180px] h-[80px] object-cover ' onClick={() => navigate('/')} src="./images/logo.png" alt="" />
                 <div className='flex gap-32 text-xl'>
                     <ul className='flex gap-24 font-extrabold'>
-                        <Link to='/region'>지역별</Link>
-                        <Link to='/choice'>메뉴선정</Link>
-                        <Link to='/hotplace'>핫플레이스</Link>
+                        <Link className={LINK_STYLE} to='/region'>지역별</Link>
+                        <Link className={LINK_STYLE} to='/choice'>메뉴선정</Link>
+                        <Link className={LINK_STYLE} to='/hotplace'>핫플레이스</Link>
                     </ul>
                     <ul className='flex gap-12'>
-                        <Link to='/login'>로그인</Link>
-                        <Link to='/signup'>회원가입</Link>
+                        <Link className={`${LINK_STYLE} font-bold`} to='/login'>로그인</Link>
+                        <Link className={`${LINK_STYLE} font-bold`} to='/signup'>회원가입</Link>
                     </ul>
                 </div>
             </div>
