00ea4f1d9d981463f2ff9a64bdd793a12c049968
bishoe01 <bishoe01@gmail.com>
2023-07-02 19:56:49 +0900

    section 정리

--- catchtable/src/components/TabSection.jsx
+++ catchtable/src/components/TabSection.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Location from './Location'
+import Notice from './Notice'
 
 export default function TabSection({ active }) {
     return (
-        <div className='mt-4'>
-            {active === '공지사항' && <div>공지사항</div>}
+        <div className='mt-10 w-2/3 mx-auto'>
+            {active === '공지사항' && <Notice />}
+            {active === '리뷰' && <div>리뷰</div>}
+            {active === '사진' && <div>사진</div>}
+            {active === '전화' && <div>전화</div>}
             {active === '지도' &&
                 <Location lat={"37.30028202155746"} long={"126.8393026399596"} />
             }
