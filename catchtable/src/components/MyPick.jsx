02e2cff1de98993a7cd2354a6782d1064088ee35
jessy <yaeunjess@gmail.com>
2023-07-10 14:47:31 +0900

    COMPLETE

--- catchtable/src/components/MyPick.jsx
+++ catchtable/src/components/MyPick.jsx
@@ -71,7 +71,7 @@ export default function MyPick() {
                   </Fade>
                 </Pulse>
             ))}
-            {!showAllPlaces && (
+            {!showAllPlaces && visiblePlaces.length >= 5 &&(
               <Fade>
                 <button
                   onClick={handleShowAllPlaces}>
--- catchtable/src/components/MyReservation.jsx
+++ catchtable/src/components/MyReservation.jsx
@@ -25,6 +25,8 @@ export default function MyReservation() {
     { title: '밀플랜비', image: "../images/place_PlanB.jpg", review:"97", price:"평균 5,000", star:4.2, detail:"부리또와 핫도그가 가성비 좋고 맛있기로 유명합니다." },
     { title: '포지타노', image: "../images/place_Positano.jpg", review:"64", price:"평균 15,000", star:5, detail:"스테이크와 파스타가 맛있는 분위기 좋은 식당입니다." },
     { title: '앤의식탁', image: "../images/place_Ann.jpg", review:"60", price:"평균 10,000", star:4, detail:"앤의 식탁만의 덮밥이 맛있고, 파스타 또한 맛있는 분위기 맛집입니다." },
+    { title: '밀플랜비', image: "../images/place_PlanB.jpg", review:"97", price:"평균 5,000", star:4.2, detail:"부리또와 핫도그가 가성비 좋고 맛있기로 유명합니다." },
+    { title: '한우궁', image: "../images/place_Hanugoong.jpg", review:"85", price:"평균 7,000", star:3.5, detail:"육개장 칼국수와 곰탕 그리고 만두로 유명한 식당입니다." },
 ];
 
   const [showAllPlaces, setShowAllPlaces] = useState(false);
@@ -45,11 +47,9 @@ export default function MyReservation() {
       <Fade>
             <div className='grid grid-cols-3 z-0 gap-8 mb-20'>
                 {visiblePlaces.map((place, index) => (
-                <Pulse when={selectedCard === index}>
+                <Pulse when={selectedCard === index} key={index}>
                   <Fade>
                     <PlaceCard
-                        key={index}
-
                         title={place.title}
                         image={place.image}
                         detail={place.detail}
@@ -64,7 +64,7 @@ export default function MyReservation() {
                   </Fade>
                 </Pulse>
             ))}
-            {!showAllPlaces && (
+            {!showAllPlaces && visiblePlaces.length >= 5 &&(
              <Fade>
               <button
                 onClick={handleShowAllPlaces}>
